c855fe23765027e4b8a7ff2a91616df96643bffa
John DaCorte <john.dacorte@gmail.com>
2022-01-01 19:32:46 -0500

    Initial Commit

---
+++ iExpense2/iExpense2App.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense2App.swift
+//  iExpense2
+//
+//  Created by John DaCorte on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpense2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
